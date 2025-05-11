a16c51670eadd5c4c982eb16ec46119bcf98a4df
Sebastián Rodríguez Tencio <sebastian.rodrigueztencio@ucr.ac.cr>
2025-05-10 19:06:03 -0600

    feat: add profile screen and protect it with middleware

---
+++ ucrconnect-dashboard/src/app/(dashboard)/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+  return (
+    <div>
+      <h1 className="text-gray-800">Mi Perfil</h1>
+      <p className="text-gray-800">Información del usuario...</p>
+    </div>
+  );
+}
