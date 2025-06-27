060ee3b6a516cabfacd1f945f79f8cb905a89e72
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2025-06-27 13:53:54 -0600

    fix(ui): standarized font to mobile

--- ucrconnect-dashboard/src/app/layout.tsx
+++ ucrconnect-dashboard/src/app/layout.tsx
@@ -1,18 +1,28 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+//import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Providers from "@/lib/providers";
 import { Toaster } from 'react-hot-toast';
 
+const poppins = Poppins({
+    variable: "--font-poppins",
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+/*
 const geistSans = Geist({
     variable: "--font-geist-sans",
     subsets: ["latin"],
 });
 
+
 const geistMono = Geist_Mono({
     variable: "--font-geist-mono",
     subsets: ["latin"],
 });
+*/
 
 export const metadata: Metadata = {
     title: "UCR Connect",
@@ -26,7 +36,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <body className={`${poppins.variable} antialiased`}>
                 <Providers>{children}</Providers>
                 <Toaster position="top-center" />
             </body>
