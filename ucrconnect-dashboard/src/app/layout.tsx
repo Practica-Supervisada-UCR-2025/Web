c71fcb4c97fc4955d2a3bf29ac33eeaaf4812167
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2025-06-27 12:43:01 -0600

    fix(ui): favicon and correct metadata

--- ucrconnect-dashboard/src/app/layout.tsx
+++ ucrconnect-dashboard/src/app/layout.tsx
@@ -15,8 +15,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "UCRConnect",
-    description: "UCRConnect",
+    title: "UCR Connect",
+    description: "UCR Connect",
 };
 
 export default function RootLayout({
